698135a7d7bf12f35d3f5447adafaea76972b61a
Darksidewalker <>
2026-06-13 10:54:54 +0200

    automatic brower open

--- cmd/dasiwa/main.go
+++ cmd/dasiwa/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
 
 	"dasiwa-quant-station/internal/app"
 )
@@ -11,6 +17,69 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("DaSiWa Quant Station listening on %s", server.Addr())
-	log.Fatal(server.ListenAndServe())
+	addr := server.Addr()
+	log.Printf("DaSiWa Quant Station listening on %s", addr)
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- server.ListenAndServe()
+	}()
+
+	if os.Getenv("DASIWA_NO_BROWSER") == "" {
+		if err := openBrowserWhenReady(addr, errCh); err != nil {
+			if startErr, ok := err.(serverStartError); ok {
+				log.Fatal(startErr.err)
+			}
+			log.Printf("Browser did not open automatically: %v", err)
+			log.Printf("Open %s in your browser.", addr)
+		}
+	}
+
+	log.Fatal(<-errCh)
+}
+
+func openBrowserWhenReady(url string, errCh <-chan error) error {
+	client := http.Client{Timeout: 500 * time.Millisecond}
+	deadline := time.After(5 * time.Second)
+	ticker := time.NewTicker(150 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case err := <-errCh:
+			return serverStartError{err: err}
+		case <-deadline:
+			return fmt.Errorf("server did not become ready in time")
+		case <-ticker.C:
+			resp, err := client.Get(url)
+			if err == nil {
+				_ = resp.Body.Close()
+				return openBrowser(url)
+			}
+		}
+	}
+}
+
+type serverStartError struct {
+	err error
+}
+
+func (e serverStartError) Error() string {
+	return e.err.Error()
+}
+
+func openBrowser(url string) error {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("open", url)
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	default:
+		if os.Getenv("DISPLAY") == "" && os.Getenv("WAYLAND_DISPLAY") == "" {
+			return fmt.Errorf("no desktop session detected")
+		}
+		cmd = exec.Command("xdg-open", url)
+	}
+	return cmd.Start()
 }
